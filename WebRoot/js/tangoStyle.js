6c71f1b995295bf8a9abb32c0281e38cae701d6c
ZhuTango <zhujianshansan@163.com>
2014-09-03 23:32:07 +0800

    add table

--- WebRoot/js/tangoStyle.js
+++ WebRoot/js/tangoStyle.js
@@ -428,3 +428,28 @@ var picker = new Pikaday({
     maxDate: new Date('2020-12-31'),
     yearRange: [2000, 2020]
 });
+
+
+
+
+/*!
+ * 方法：关于table方法
+ * Date: 2014.9.3
+ */
+$("#grid-data").bootgrid({
+    ajax: true,
+    post: function ()
+    {
+        /* To accumulate custom parameter with the request object */
+        return {
+            id: "b0df282a-0d67-40e5-8558-c9e93b7befed"
+        };
+    },
+    url: "/api/data/basic",
+    formatters: {
+        "link": function(column, row)
+        {
+            return "<a href=\"#\">" + column.id + ": " + row.id + "</a>";
+        }
+    }
+});
