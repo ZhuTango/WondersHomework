7a040daca91e7dd63e4da257a90b84e513c6f91c
ZhuTango <zhujianshansan@163.com>
2014-09-04 23:43:44 +0800

    add anothergrid,but failed

--- WebRoot/js/tangoStyle.js
+++ WebRoot/js/tangoStyle.js
@@ -438,8 +438,7 @@ var picker = new Pikaday({
  */
 $("#grid-data").bootgrid({
     ajax: true,
-    post: function ()
-    {
+    post: function () {
         /* To accumulate custom parameter with the request object */
         return {
             id: "b0df282a-0d67-40e5-8558-c9e93b7befed"
@@ -447,9 +446,81 @@ $("#grid-data").bootgrid({
     },
     url: "/api/data/basic",
     formatters: {
-        "link": function(column, row)
-        {
+        "link": function (column, row) {
             return "<a href=\"#\">" + column.id + ": " + row.id + "</a>";
         }
     }
+});
+
+
+
+
+
+
+
+
+$(function () {
+    var data = [];
+
+    var grid = $("#grid").bootgrid({
+        pagination: 3,
+        selection: true,
+        multiSelect: true,
+        formatters: {
+            "quantity": function (column, row) {
+                return "<input id=\"" + row.id + "-quantity\" type=\"text\" class=\"quantity\" value=\"" + row.quantity + "\" />";
+            }
+        }
+    }).on("loaded.rs.jquery.bootgrid", function (e) {
+        // Resets the selected data array on reload, searching, sorting or changing page
+        data = [];
+
+        grid.on("keyup", "input.quantity", function () {
+            e.stopPropagation();
+
+            // Array.first is an extension of bootgrid
+            var $this = $(this),
+                rowId = +$this.attr("id").split("-")[0], // "+" to convert the a string to an integer
+                item = data.first(function (item) {
+                    return item.id === rowId;
+                });
+            if (item != null) {
+                item.quantity = $this.val();
+            }
+        });
+    }).on("selected.rs.jquery.bootgrid", function (e, selectedRows) {
+        var row, quantity;
+        for (var i = 0; i < selectedRows.length; i++) {
+            row = selectedRows[i];
+
+            // Array.contains is an extension of bootgrid
+            if (!data.contains(function (item) {
+                return item.id === row.id;
+            })) {
+                quantity = grid.find("#" + row.id + "-quantity").val();
+                data.push({
+                    id: row.id,
+                    quantity: quantity || 0
+                });
+            }
+        }
+    }).on("deselected.rs.jquery.bootgrid", function (e, deselectedRows) {
+        var row;
+        for (var i = 0; i < deselectedRows.length; i++) {
+            row = deselectedRows[i];
+            for (var j = 0; j < data.length; j++) {
+                if (data[j].id === row.id) {
+                    data.splice(j, 1);
+                    return;
+                }
+            }
+        }
+    });
+
+    $("#send").on("click", function () {
+        var params = $.param({
+            "": data
+        });
+        alert(decodeURIComponent(params));
+    });
 });
